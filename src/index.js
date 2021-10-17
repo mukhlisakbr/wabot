f905d7968b81f4df1c6ac571696bc44473bdb2d0
mukhlisakbr <mukhlis3389@gmail.com>
2021-10-17 19:16:01 +0700

    feat: add image to sticker, video to sticker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-const wa = require('@open-wa/wa-automate');
+const { create, decryptMedia } = require('@open-wa/wa-automate');
+const signale = require('signale');
+// const mime = require('mime-types');
+// const fs = require('fs');
+
+const stickerMetadata = {
+  pack: 'bikin nya di',
+  author: 'wabot.my.id',
+};
 
 function start(client) {
   client.onMessage(async (message) => {
-    if (message.body === 'Hi') {
-      await client.sendText(message.from, '👋 Hello!');
+    if (message.isGroupMsg) return;
+    if (!message.isMedia) return;
+    if (!message.caption.includes('stiker')) return;
+    signale.info(
+      message.from,
+      message.sender.pushname,
+      message.type,
+      message.caption
+    );
+    if (message.mimetype) {
+      const mediaData = await decryptMedia(message);
+      if (message.type === 'video') {
+        await client.sendMp4AsSticker(
+          message.from,
+          mediaData,
+          null,
+          stickerMetadata
+        );
+      } else {
+        await client.sendImageAsSticker(
+          message.from,
+          `data:${message.mimetype};base64,${mediaData.toString('base64')}`,
+          stickerMetadata
+        );
+      }
     }
   });
 }
 
-wa.create({
+create({
   sessionId: 'WABOT',
   authTimeout: 60,
   blockCrashLogs: true,
