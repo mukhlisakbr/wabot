37ac034a789fd2ca68106882567c0dbbe6cccd60
mukhlisakbr <mukhlis3389@gmail.com>
2021-10-22 19:06:22 +0700

    refactor: move to separate file

---
+++ src/menu.js
@@ -0,0 +1,6 @@
+module.exports = `
+Send a picture or video you will get a sticker. Give a caption to use select mode or crop type (optional).
+
+Supported caption:
+- circle, keep scale, top, right top, right, right bottom, bottom, left bottom, left, left top, north, northeast, east, southeast, south, southwest, west, northwest, center, centre, entropy, attention.
+`;
