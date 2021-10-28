904ae12383d1f7d301dbe354f4e5f5764aaab990
mukhlisakbr <mukhlis3389@gmail.com>
2021-10-28 18:47:32 +0700

    fix: handle message condition

--- src/messageHandler.js
+++ src/messageHandler.js
@@ -5,89 +5,94 @@ const menu = require('./menu');
 module.exports = async (client, message) => {
   const { isGroupMsg, isMedia, caption, from, sender, type, mimetype, body } =
     message;
+  // throw group msg
   if (isGroupMsg) return;
-  if (body.toLowerCase() === 'menu') {
+  // text
+  if (body && body.toLowerCase() === 'menu') {
     signale.info(from, sender.pushname, type, body);
     await client.sendText(from, menu);
-  } else {
-    if (!isMedia) return;
+  }
+  // media
+  if (isMedia) {
     signale.info(from, sender.pushname, type, caption);
     let stickerMetadata = {
       pack: 'bot stiker wa',
       author: '@wabot_id',
     };
-    switch (caption.toLowerCase()) {
-      case 'circle':
-        stickerMetadata = { ...stickerMetadata, circle: true };
-        break;
-      case 'keep scale':
-        stickerMetadata = { ...stickerMetadata, keepScale: true };
-        break;
-      case 'top':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'top' };
-        break;
-      case 'right top':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'right top' };
-        break;
-      case 'right':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'right' };
-        break;
-      case 'right bottom':
-        stickerMetadata = {
-          ...stickerMetadata,
-          cropPosition: 'right bottom',
-        };
-        break;
-      case 'bottom':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'bottom' };
-        break;
-      case 'left bottom':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'left bottom' };
-        break;
-      case 'left':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'left' };
-        break;
-      case 'left top':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'left top' };
-        break;
-      case 'north':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'north' };
-        break;
-      case 'northeast':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'northeast' };
-        break;
-      case 'east':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'east' };
-        break;
-      case 'southeast':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'southeast' };
-        break;
-      case 'south':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'south' };
-        break;
-      case 'southwest':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'southwest' };
-        break;
-      case 'west':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'west' };
-        break;
-      case 'northwest':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'northwest' };
-        break;
-      case 'center':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'center' };
-        break;
-      case 'centre':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'center' };
-        break;
-      case 'entropy':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'entropy' };
-        break;
-      case 'attention':
-        stickerMetadata = { ...stickerMetadata, cropPosition: 'attention' };
-        break;
-      default:
-        break;
+    if (caption) {
+      switch (caption.toLowerCase()) {
+        case 'circle':
+          stickerMetadata = { ...stickerMetadata, circle: true };
+          break;
+        case 'keep scale':
+          stickerMetadata = { ...stickerMetadata, keepScale: true };
+          break;
+        case 'top':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'top' };
+          break;
+        case 'right top':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'right top' };
+          break;
+        case 'right':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'right' };
+          break;
+        case 'right bottom':
+          stickerMetadata = {
+            ...stickerMetadata,
+            cropPosition: 'right bottom',
+          };
+          break;
+        case 'bottom':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'bottom' };
+          break;
+        case 'left bottom':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'left bottom' };
+          break;
+        case 'left':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'left' };
+          break;
+        case 'left top':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'left top' };
+          break;
+        case 'north':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'north' };
+          break;
+        case 'northeast':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'northeast' };
+          break;
+        case 'east':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'east' };
+          break;
+        case 'southeast':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'southeast' };
+          break;
+        case 'south':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'south' };
+          break;
+        case 'southwest':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'southwest' };
+          break;
+        case 'west':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'west' };
+          break;
+        case 'northwest':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'northwest' };
+          break;
+        case 'center':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'center' };
+          break;
+        case 'centre':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'center' };
+          break;
+        case 'entropy':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'entropy' };
+          break;
+        case 'attention':
+          stickerMetadata = { ...stickerMetadata, cropPosition: 'attention' };
+          break;
+        default:
+          break;
+      }
     }
     if (mimetype) {
       const mediaData = await decryptMedia(message);
