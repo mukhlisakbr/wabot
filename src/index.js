35f9e271f1fc7768a07c1b6d3dffc650fb0770ef
mukhlisakbr <mukhlis3389@gmail.com>
2021-11-05 16:09:53 +0700

    refactor: update config for linux [skip ci]

--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ create({
   logConsoleErrors: true,
   pQueueDefault: { concurrency: 1 },
   restartOnCrash: start,
-  chromiumArgs: ['--disable-dev-shm-usage'],
+  chromiumArgs: [
+    '--disable-dev-shm-usage',
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ],
 })
   .then((client) => start(client))
   .catch((err) => signale.error(err));
