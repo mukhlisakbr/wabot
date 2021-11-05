2a590b28d3e3601da4c4c09b472e45f379df5dac
mukhlisakbr <mukhlis3389@gmail.com>
2021-11-05 15:11:45 +0700

    refactor: skip session save [skip ci]

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 const { create } = require('@open-wa/wa-automate');
 const signale = require('signale');
 const messageHandler = require('./messageHandler');
+require('dotenv').config();
 
 const start = (client) => {
   client.onMessage((message) => messageHandler(client, message));
 };
 
 create({
-  sessionId: 'WABOT',
+  skipSessionSave: true,
   disableSpins: true,
   qrTimeout: 0,
-  authTimeout: 60,
+  authTimeout: 0,
   cacheEnabled: false,
   killClientOnLogout: true,
   killProcessOnTimeout: true,
   killProcessOnBrowserClose: true,
   logConsoleErrors: true,
-  pQueueDefault: { concurrency: 2 },
+  pQueueDefault: { concurrency: 1 },
   restartOnCrash: start,
   chromiumArgs: ['--disable-dev-shm-usage'],
 })
